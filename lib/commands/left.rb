d35d7786dde2c57611fd38cbaa7ec30abe0760f9
Eugene Dyko <yevdyko@gmail.com>
2019-06-02 18:36:25 +0300

    Add left command

--- lib/commands/left.rb
+++ lib/commands/left.rb
@@ -4,5 +4,10 @@ require_relative '../command'
 
 module Commands
   class Left < Command
+    def call(table:, robot:)
+      return unless robot.placed?
+
+      robot.turn(:left)
+    end
   end
 end
--- spec/commands/left_spec.rb
+++ spec/commands/left_spec.rb
@@ -3,4 +3,16 @@
 require 'commands/left'
 
 describe Commands::Left do
+  subject(:left) { described_class.new(type: type) }
+  let(:type) { 'left' }
+  let(:table) { instance_double('Table') }
+  let(:robot) { instance_double('Robot', placed?: true) }
+
+  describe '#call' do
+    it 'rotates the placed robot on a table in the specified direction' do
+      expect(robot).to receive(:turn).with(:left)
+
+      left.call(table: table, robot: robot)
+    end
+  end
 end
