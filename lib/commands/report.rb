d5d9b9541a555269df7b3f08f2b2eaed9cf8155d
Eugene Dyko <yevdyko@gmail.com>
2019-06-02 18:52:59 +0300

    Add report command

--- lib/commands/report.rb
+++ lib/commands/report.rb
@@ -4,5 +4,8 @@ require_relative '../command'
 
 module Commands
   class Report < Command
+    def call(table:, robot:)
+      puts "#{robot.x_coordinate},#{robot.y_coordinate},#{robot.facing.upcase}"
+    end
   end
 end
--- spec/commands/report_spec.rb
+++ spec/commands/report_spec.rb
@@ -3,4 +3,14 @@
 require 'commands/report'
 
 describe Commands::Report do
+  subject(:report) { described_class.new(type: type) }
+  let(:type) { 'report' }
+  let(:table) { instance_double('Table') }
+  let(:robot) { instance_double('Robot', x_coordinate: 0, y_coordinate: 0, facing: :north) }
+
+  describe '#call' do
+    it 'reports the x-coordinate, y-coordinate and facing of the robot' do
+      expect { report.call(table: table, robot: robot) }.to output("0,0,NORTH\n").to_stdout
+    end
+  end
 end
