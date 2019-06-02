281e2295f88fd484c3cb682f0c372e28be20d64a
Eugene Dyko <yevdyko@gmail.com>
2019-06-02 17:43:19 +0300

    Add move command

--- lib/commands/move.rb
+++ lib/commands/move.rb
@@ -4,5 +4,14 @@ require_relative '../command'
 
 module Commands
   class Move < Command
+    def call(table:, robot:)
+      return unless robot.placed?
+
+      x_coordinate, y_coordinate = robot.next_move
+      return unless table.valid_position?(x_coordinate, y_coordinate)
+
+      robot.x_coordinate = x_coordinate
+      robot.y_coordinate = y_coordinate
+    end
   end
 end
--- spec/commands/move_spec.rb
+++ spec/commands/move_spec.rb
@@ -3,4 +3,25 @@
 require 'commands/move'
 
 describe Commands::Move do
+  subject(:move) { described_class.new(type: type) }
+  let(:type) { 'move' }
+  let(:table) { instance_double('Table', valid_position?: true) }
+  let(:robot) do
+    instance_double(
+      'Robot',
+      :x_coordinate= => 0,
+      :y_coordinate= => 1,
+      placed?: true,
+      next_move: [0, 1]
+    )
+  end
+
+  describe '#call' do
+    it 'moves the placed robot on a table to the valid position' do
+      expect(robot).to receive(:x_coordinate=).with(0)
+      expect(robot).to receive(:y_coordinate=).with(1)
+
+      move.call(table: table, robot: robot)
+    end
+  end
 end
