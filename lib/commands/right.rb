3d5b239b332387fa00337fe9324d4506d536fb03
Eugene Dyko <yevdyko@gmail.com>
2019-06-02 18:33:52 +0300

    Add right command

--- lib/commands/right.rb
+++ lib/commands/right.rb
@@ -4,5 +4,10 @@ require_relative '../command'
 
 module Commands
   class Right < Command
+    def call(table:, robot:)
+      return unless robot.placed?
+
+      robot.turn(:right)
+    end
   end
 end
--- spec/commands/right_spec.rb
+++ spec/commands/right_spec.rb
@@ -3,4 +3,16 @@
 require 'commands/right'
 
 describe Commands::Right do
+  subject(:right) { described_class.new(type: type) }
+  let(:type) { 'right' }
+  let(:table) { instance_double('Table') }
+  let(:robot) { instance_double('Robot', placed?: true) }
+
+  describe '#call' do
+    it 'rotates the placed robot on a table in the specified direction' do
+      expect(robot).to receive(:turn).with(:right)
+
+      right.call(table: table, robot: robot)
+    end
+  end
 end
